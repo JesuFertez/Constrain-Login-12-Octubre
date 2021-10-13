c5bae81bdaa1beb4c79fa4090051aaf7a27a68d1
JesuFertez <maria.fernandez.cortez@gmail.com>
2021-10-13 20:15:31 -0300

    Agregando libreria Picasso

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView ivAvatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initWidget();
+        asignarImagen();
+    }
+    public void asignarImagen(){
+
+        Picasso.get().load("https://static.wikia.nocookie.net/drslump/images/4/4f/Arale_by_sauron88-d5a00n0.png/revision/latest?cb=20180106050156&path-prefix=es").into(ivAvatar);
+    }
+    public void initWidget(){
+
+        ivAvatar=findViewById(R.id.iv_avatar);
     }
 }
