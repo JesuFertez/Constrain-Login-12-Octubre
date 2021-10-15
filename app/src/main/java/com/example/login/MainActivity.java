42fcdae35a6633f44ee385b6bb4794fd89b7902d
JesuFertez <maria.fernandez.cortez@gmail.com>
2021-10-14 22:22:16 -0300

    Uso de viewBinding

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -3,27 +3,46 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.example.login.databinding.ActivityMainBinding;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView ivAvatar;
 
+    //private ImageView ivAvatar;
+    //private EditText nombre, apellido, mail, contraseña;
+    public static final String URL= "https://static.wikia.nocookie.net/drslump/images/4/4f/Arale_by_sauron88-d5a00n0.png/revision/latest?cb=20180106050156&path-prefix=es";
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        initWidget();
+        initBinding();
+        setContentView(binding.getRoot());
+
         asignarImagen();
+        loadValues();
     }
     public void asignarImagen(){
 
-        Picasso.get().load("https://static.wikia.nocookie.net/drslump/images/4/4f/Arale_by_sauron88-d5a00n0.png/revision/latest?cb=20180106050156&path-prefix=es").into(ivAvatar);
+        Picasso.get().load(URL).into(binding.ivAvatar);
     }
-    public void initWidget(){
+    public void initBinding(){
 
-        ivAvatar=findViewById(R.id.iv_avatar);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        /*ivAvatar=findViewById(R.id.iv_avatar);
+        nombre=findViewById(R.id.edt_nombre_persona);
+        apellido=findViewById(R.id.edt_apellido_persona);
+        mail=findViewById(R.id.edt_Mail_persona);
+        contraseña=findViewById(R.id.edt_contraseña_persona);*/
+
     }
+    public void loadValues(){
+        binding.edtNombrePersona.setText("María Jesús");
+        binding.edtApellidoPersona.setText("Fernández");
+    }
+
+
 }
